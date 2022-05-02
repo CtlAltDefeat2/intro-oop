6562d0edad3fb51bd0f20bb5d3f7c489e1371bad
TejPatel11 <tejpatel798@gmail.com>
2022-05-02 15:30:35 -0400

    update

---
+++ src/main/java/com/xpanxion/myapp/student24/Person.java
@@ -0,0 +1,73 @@
+package com.xpanxion.myapp.student24;
+
+public class Person {
+        private String firstName;
+        private String lastName;
+        private byte age;
+        private int ssn;
+        private static int personCount = 0;
+        Person(){
+            personCount++;
+        }
+        Person(String firstName, String lastName){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.personCount++;
+        }
+        Person(String firstName, String lastName, byte age){
+            this.firstName = firstName;
+            this.lastName=lastName;
+            this.age =age;
+            this.personCount++;
+        }
+        Person(String firstName, String lastName, byte age, int ssn){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.ssn = ssn;
+            this.personCount++;
+        }
+        public String speak(){
+            if(firstName == " "){
+                System.out.println("I dont have a name");
+            }
+            return ("My name is " + firstName + " " + lastName + " and I am " + age + " years old.");
+        }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+    public static int getPersonCount(){
+            return personCount;
+    }
+}
+
+
