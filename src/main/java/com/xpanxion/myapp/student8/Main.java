cfd0b8174d284306b46f613a82d0b49f381786de
DominicDiMarcello <103143836+DominicDiMarcello@users.noreply.github.com>
2022-05-03 09:33:05 -0400

    Update

--- src/main/java/com/xpanxion/myapp/student8/Main.java
+++ src/main/java/com/xpanxion/myapp/student8/Main.java
@@ -14,9 +14,9 @@ public class Main {
         var p2 = new Person("Jon", "Smith");
         var p3 = new Person("Mary", "Jane", (byte) 12, 123456789);
 
-//        System.out.println(p1.speak());
-//        System.out.println(p2.speak());
-//        System.out.println(p3.speak());
+        System.out.println(p1.speak());
+        System.out.println(p2.speak());
+        System.out.println(p3.speak());
 
         System.out.println(p1);
         System.out.println(p2);
--- src/main/java/com/xpanxion/myapp/student8/Person.java
+++ src/main/java/com/xpanxion/myapp/student8/Person.java
@@ -6,20 +6,21 @@ public class Person {
     String lastName;
     byte Age;
     int SSN;
+    int type;
 
     private static int counter;
 
 
     public Person() {
         counter++;
-        System.out.println("N/A");
+        type = 1;
     }
 
     public Person(String firstname, String lastname) {
         this.firstName = firstname;
         this.lastName = lastname;
         counter++;
-        System.out.println(firstName + "" + lastName);
+        type = 2;
     }
 
     public Person(String firstname, String lastname, byte age, int ssn) {
@@ -28,16 +29,42 @@ public class Person {
         this.Age = age;
         this.SSN = ssn;
         counter++;
-        System.out.println(firstName + " " + lastName + " " + SSN);
+        type = 3;
     }
 
 
-//    public speak() {
-//
-//    }
+    public String speak() {
+        if (type == 1) {
+            return "I don't have a name";
+        }
+        if (type == 2) {
+            return "My name is " + firstName + " " + lastName;
+        }
+        if (type == 3) {
+            return "My name is " + firstName + " " + lastName + " and I am " + Age + " years old.";
+        }
+        return null;
+    }
+
 
     public static int getPersonCount() {
         return counter;
+    }
+
+    @Override
+    public String toString() {
+
+        if (type == 1) {
+            return "N/A";
+        }
+        if (type == 2) {
+            return firstName + " " + lastName;
+        }
+        if (type == 3) {
+            String lastFourDigits = String.valueOf(SSN);
+            return firstName + " " + lastName + " " + lastFourDigits.substring(lastFourDigits.length()-4);
+        }
+        return null;
     }
 
 }
