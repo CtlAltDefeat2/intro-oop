241f1861ffe5f34651d6e89a029bcdfba10ca1f5
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-05-03 14:07:52 -0400

    ex1

--- src/main/java/com/xpanxion/myapp/student11/Main.java
+++ src/main/java/com/xpanxion/myapp/student11/Main.java
@@ -2,29 +2,30 @@ package com.xpanxion.myapp.student11;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
+           ex1();
         // ex2();
         // ex3();
         // ex4();
         // ex5();
     }
         private static void ex1() {
-        var p1 = new Person();
-        var p2 = new Person("Jon", "Smith");
-        var p3 = new Person("Mary", "Jane", (byte) 12, 123456789);
-    }}
-
-/*System.out.println(p1.speak());
-System.out.println(p2.speak());
-System.out.println(p3.speak());
-
-System.out.println(p1);
-System.out.println(p2);
-System.out.println(p3);
-
-System.out.println("Person count: " + Person.getPersonCount());
+            var p1 = new Person();
+            var p2 = new Person("Jon", "Smith");
+            var p3 = new Person("Mary", "Jane", (byte) 12, 123456789);
 
 
+            System.out.println(p1.speak());
+            System.out.println(p2.speak());
+            System.out.println(p3.speak());
+
+            System.out.println(p1);
+            System.out.println(p2);
+            System.out.println(p3);
+
+            System.out.println("Person count: " + Person.getPersonCount());
+
+
+        }
 
 
 
@@ -53,4 +54,4 @@ System.out.println("Person count: " + Person.getPersonCount());
     private static void ex5() {
         System.out.println("Todo...");
     }
-} */
+}
--- src/main/java/com/xpanxion/myapp/student11/Person.java
+++ src/main/java/com/xpanxion/myapp/student11/Person.java
@@ -9,32 +9,50 @@ private String lastName;
 private Byte age;
 private Integer ssn;
 
+private static int personCount = 0;
+
 //3 constructors. first thing that runs.
- Person() {
- }
+ Person() { this.personCount++;}
+
  Person (String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-
+this.personCount++;
     }
 Person (String firstName, String lastName, Byte age, Integer ssn) {
             this.firstName = firstName;
             this.lastName = lastName;
             this.age = age;
             this.ssn = ssn;
+            this.personCount++;
         }
 //methods - the layout of what we want ran. handle the 3 use cases.
+
+    public static int getPersonCount() {return personCount++;}
             public String speak() {
-            return "My name is: " + firstName;
+     var message = "";
+     if (firstName == null && lastName == null) {
+         message = "I don't have a name.";
+     } else if (age == null) {
+         message = "My name is " + firstName + " " + lastName;
+     } else {
+         message = "My name is " + firstName + " " + lastName + "and I am " + age + "years old.";
+     }
+            return message;
             }
-        }
+        //override
             @Override
             public String toString() {
-                return "Person{" +
-                        "firstName='" + firstName + '\'' +
-                        ", lastName='" + lastName + '\'' +
-                        ", age=" + age +
-                        ", ssn=" + ssn +
-                        '}';
+     String reVal = "";
+     if (firstName == null) {
+         reVal = "N/A";
+     } else if (ssn == null) {
+         reVal = firstName + " " + lastName;
+     }
+     if (ssn != null) {
+         var ssnString = Integer.toString(ssn).substring(5);
+         reVal = firstName + " " + lastName + " " + ssnString;
+     }
+     return reVal;
         }
 }
