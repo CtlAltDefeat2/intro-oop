4433532e2a90ea067372b7db889f5c0ba2df1fc7
Donna Lemon <donnalemon1@gmail.com>
2022-05-03 14:22:05 -0400

    Exercise 1.

--- src/main/java/com/xpanxion/myapp/student9/Main.java
+++ src/main/java/com/xpanxion/myapp/student9/Main.java
@@ -2,32 +2,32 @@ package com.xpanxion.myapp.student9;
 
 public class Main {
     public static void main(String[] args) {
+
         ex1();
         // ex2();
         // ex3();
         // ex4();
         // ex5();
     }
+        private static void ex1() {
+            var p1 = new Person();
+            var p2 = new Person("Jon", "Smith");
+            var p3 = new Person("Mary", "Jane", (byte) 12, 123456789);
 
-    private static void ex1()
-    var p1 = new Person();
-    var p2 = new Person("Jon", "Smith");
-    var p3 = new Person("Mary", "Jane", (byte) 12, 123456789);
+            System.out.println(p1.speak());
+            System.out.println(p2.speak());
+            System.out.println(p3.speak());
 
-System.out.println(p1.speak());
-System.out.println(p2.speak());
-System.out.println(p3.speak());
+            System.out.println(p1);
+            System.out.println(p2);
+            System.out.println(p3);
 
-System.out.println(p1);
-System.out.println(p2);
-System.out.println(p3);
+            System.out.println("Person count: " + Person.getPersonCount());
 
-System.out.println("Person count: " + Person.getPersonCount());
-        System.out.println("Todo...");
+        }
+
     }
-
-
-    private static void ex2() {
+  /*  private static void ex2() {
         System.out.println("Todo...");
     }
 
@@ -42,4 +42,4 @@ System.out.println("Person count: " + Person.getPersonCount());
     private static void ex5() {
         System.out.println("Todo...");
     }
-}
+}*/
