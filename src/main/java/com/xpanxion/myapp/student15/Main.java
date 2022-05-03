c8dea72300bd5cc418737caad1aa8b95af47cd76
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-05-03 14:43:03 -0400

    upate

--- src/main/java/com/xpanxion/myapp/student15/Main.java
+++ src/main/java/com/xpanxion/myapp/student15/Main.java
@@ -2,8 +2,8 @@ package com.xpanxion.myapp.student15;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
-        // ex2();
+        //ex1();
+         ex2();
         // ex3();
         // ex4();
         // ex5();
