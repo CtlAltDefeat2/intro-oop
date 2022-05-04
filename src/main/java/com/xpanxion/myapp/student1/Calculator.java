e5dd24065a530f480dd272db8844435ce7ea965f
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-05-04 15:13:18 -0400

    update ex4

---
+++ src/main/java/com/xpanxion/myapp/student1/Calculator.java
@@ -0,0 +1,74 @@
+package com.xpanxion.myapp.student1;
+//Extends someSuperClass //Implements some Interfaces separated by commas {
+//Data Members
+//Constructors
+//Methods
+
+
+import java.util.ArrayList;
+
+public class Calculator implements Calculatable {
+
+//
+// Constructors
+//
+    public Calculator(ArrayList<String> history) {
+        this.history = history;
+    }
+
+//
+//Methods
+//
+
+    private ArrayList<String> history = new ArrayList<String>();
+
+    @Override
+    public Integer add(Integer number1, Integer number2) {
+        int addition = 0;
+        addition = number1 + number2;
+        String result = number1 + "+" + number2 + "=" + addition + "\n";
+        this.history.add(result);
+        return addition;
+    }
+
+    @Override
+    public Integer subtract(Integer number1, Integer number2) {
+        int subtraction = 0;
+        subtraction = number1 - number2;
+        String result = number1 + "-" + number2 + "=" + subtraction + "\n";
+        this.history.add(result);
+        return subtraction;
+    }
+
+    @Override
+    public Integer multiply(Integer number1, Integer number2) {
+        int multiplication = 0;
+        multiplication = number1 * number2;
+        String result = number1 + "*" + number2 + "=" + multiplication + "\n";
+        this.history.add(result);
+        return multiplication;
+    }
+
+    @Override
+    public Integer divide(Integer number1, Integer number2) {
+        int division = 0;
+        division = number1 / number2;
+        String result = number1 + "/" + number2 + "=" + division + "\n";
+        this.history.add(result);
+        return division;
+    }
+
+    @Override
+    public String getHistory() {
+        var retVal = "";
+        //var retVal = "1+2 = 3\n4-2 = 2\n10 /5 =2\n";
+        for (int i = 0; i<history.size(); i++) {
+            retVal += history.get(i);
+        }
+        return retVal;
+    }
+}
+
+
+
+
