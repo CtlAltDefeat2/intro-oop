df01e2c88042ca4bde180da3716901c74bd7b0f1
Zachary Porras <ztp567@gmail.com>
2022-05-03 23:35:35 -0400

    ex1update

--- src/main/java/com/xpanxion/myapp/student26/Person.java
+++ src/main/java/com/xpanxion/myapp/student26/Person.java
@@ -1,23 +1,64 @@
 package com.xpanxion.myapp.student26;
 
-import javax.sound.midi.Soundbank;
-import java.util.Scanner;
-
 public class Person {
 
-    public static void main(String[] args) {
+    private String firstName;
+    private String lastName;
+    private byte age=0;
+    private Integer SSN =0;
+    private static Integer person = 0;
 
-        String firstname;
-        String lastname;
+    Person() {
+        this.person++;
+    }
 
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.person++;
+    }
 
+    Person(String firstName, String lastName, Byte age, Integer SSN) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.SSN = SSN;
+        this.person++;
+    }
 
+    public static Integer getPersonCount() {
+        return person++;
+    }
 
+    public String speak() {
+
+        String retval = "I dont have a name";
+        if (firstName != null && lastName != null && age == 0) {
+            retval = "My name is " + firstName + " " + lastName;
+        }
+        if (firstName != null && lastName != null && age > 0) {
+            return "My name is " + firstName + " " + lastName + " and I am " + age + " years old";
+        }
+        return retval;
+    }
+
+    private String lastFour(Integer SSN) {
+
+            return Integer.toString(SSN).substring(5);
 
     }
 
+    public String toString() {
+        String retVal = "N/A";
+        if (firstName != null && lastName != null && age == 0) {
+            retVal = firstName + " " + lastName;
+        }
+        if (firstName != null && lastName != null && SSN > 0) {
+            retVal = firstName + " " + lastName + " " + lastFour(SSN);
 
-
-
+        }
+        return retVal;
+    }
 
 }
+
