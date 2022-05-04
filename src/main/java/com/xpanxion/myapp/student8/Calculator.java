7b8340a379ff25374ebc332a46cb3cc476410115
DominicDiMarcello <103143836+DominicDiMarcello@users.noreply.github.com>
2022-05-04 11:21:22 -0400

    UPDATE

---
+++ src/main/java/com/xpanxion/myapp/student8/Calculator.java
@@ -0,0 +1,33 @@
+package com.xpanxion.myapp.student8;
+
+
+import java.util.ArrayList;
+
+public class Calulator implements Calculatable {
+
+
+    @Override
+    public Integer add(Integer number1, Integer number2) {
+        return null;
+    }
+
+    @Override
+    public Integer subtract(Integer number1, Integer number2) {
+        return null;
+    }
+
+    @Override
+    public Integer multiply(Integer number1, Integer number2) {
+        return null;
+    }
+
+    @Override
+    public Integer divide(Integer number1, Integer number2) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> getHistory() {
+        return null;
+    }
+}
