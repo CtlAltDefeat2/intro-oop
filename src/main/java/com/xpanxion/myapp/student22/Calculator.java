2e1db2b8ec5dc3c781d7e961f93bf898f2f1725b
sniang1 <96024696+sniang1@users.noreply.github.com>
2022-05-04 14:21:59 -0400

    ex4 update

---
+++ src/main/java/com/xpanxion/myapp/student22/Calculator.java
@@ -0,0 +1,45 @@
+package com.xpanxion.myapp.student22;
+
+import java.util.ArrayList;
+
+public class Calculator implements Calculatable{
+    private ArrayList history = new ArrayList<String>();
+    @Override
+    public Integer add(Integer number1, Integer number2) {
+        int sum = number1 + number2;
+        String histories = number1 +" +" + number2 +" =" + sum;
+        this.history.add(histories);
+        return sum;
+    }
+
+    @Override
+    public Integer subtract(Integer number1, Integer number2) {
+        int sum = number1 - number2;
+        String histories = "\n" + number1 +" -" + number2 +" =" + sum;
+        this.history.add(histories);
+        return sum;
+    }
+
+    @Override
+    public Integer multiply(Integer number1, Integer number2) {
+        int sum = number1 * number2;
+        String histories ="\n" + number1 +" *" + number2 +" =" + sum;
+        this.history.add(histories);
+        return sum;
+    }
+
+    @Override
+    public Integer divide(Integer number1, Integer number2) {
+        int sum = number1 / number2;
+        String histories ="\n" + number1 +" /" + number2 +" =" + sum;
+        this.history.add(histories);
+        return sum;
+    }
+
+    @Override
+    public ArrayList<String> getHistory() {
+
+
+        return history;
+    }
+}
