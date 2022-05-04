4fc441daf530120874d74d10b894def0688e57aa
wdelacruzl <wdelacruzl98@gmail.com>
2022-05-04 15:10:42 -0400

    Ex 4 Done Right

--- src/main/java/com/xpanxion/myapp/student25/Main.java
+++ src/main/java/com/xpanxion/myapp/student25/Main.java
@@ -2,10 +2,10 @@ package com.xpanxion.myapp.student25;
 
 public class Main { //Wendy
     public static void main(String[] args) {
-        ex1();
+//        ex1();
         // ex2()
-        // ex3();
-        // ex4();
+ //        ex3();
+         ex4();
         // ex5();
     }
 
@@ -23,22 +23,32 @@ public class Main { //Wendy
         System.out.println(p3);
 
         System.out.println("Person count: " + Person.getPersonCount());
-
+    }
 
 //        private static void ex2() {
 //        System.out.println("Todo...");
 //    }
 //
-//    private static void ex3() {
-//        System.out.println("Todo...");
-//    }
+ //       private static void ex3 () {
+  //  }
+    private static void ex4() {
+        var calculator =  new Calculator();
+        var result1 = calculator.add(1, 2);
+        var result2 = calculator.subtract(4, 2);
+        var result3 = calculator.divide(10, 5);
+        var result4 = calculator.multiply(2, 2);
 //
-//    private static void ex4() {
-//        System.out.println("Todo...");
-//    }
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(calculator.getHistory());
+    }
 //
 //    private static void ex5() {
 //        System.out.println("Todo...");
 //    }
-    };
-}
+        }
+
+
+
