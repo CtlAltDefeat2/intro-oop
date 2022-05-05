d9b1e948eb9d7b0600a0bfcf4dd6864f102ab159
Elden-Lord01 <levi.melton43@gmail.com>
2022-05-05 16:25:41 -0400

    update

--- src/main/java/com/xpanxion/myapp/student20/Calculator.java
+++ src/main/java/com/xpanxion/myapp/student20/Calculator.java
@@ -9,60 +9,29 @@ public class Calculator implements Calculatable  {
     //
     private int number1;
     private int number2;
-    private ArrayList history = new ArrayList();
+    private ArrayList<String> history = new ArrayList();
     //
     //Constructor
     //
     public Calculator(){
     }
-    Calculator(Integer number1, Integer number2){
-        this.number1 = number1;
-        this.number2 = number2;
-    }
-
-
-
     @Override
     public Integer add(Integer number1, Integer number2) {
-        this.number1 = number1;
-        this.number2 = number2;
         int sum = number1 + number2;
-        String problem = "\n" + number1 + " + " + number2 + " = " + sum;
+        String problem =  number1 + " + " + number2 + " = " + sum;
         this.history.add(problem);
         return sum;
     }
-
     @Override
     public Integer subtract(Integer number1, Integer number2) {
-        this.number1 = number1;
-        this.number2 = number2;
         int sum = number1 - number2;
         String problem = "\n" + number1 + " - " + number2 + " = " + sum;
         this.history.add(problem);
         return sum;
     }
 
-    @Override
-    public Integer multiply(Integer number1, Integer number2) {
-        this.number1 = number1;
-        this.number2 = number2;
-        int sum = number1 * number2;
-        String problem = "\n" + number1 + " * " + number2 + " = " + sum;
-        this.history.add(problem);
-        return sum;
-    }
-
-    @Override
-    public Integer divide(Integer number1, Integer number2) {
-        this.number1 = number1;
-        this.number2 = number2;
-        int sum = number1 / number2;
-        String problem = "\n" + number1 + " / " + number2 + " = " + sum;
-        this.history.add(problem);
-        return sum;
-    }
-
-    public ArrayList<String> getHistory() {
-        return history;
+    public String getHistory(){
+        String listString = String.join("",history);
+        return listString;
     }
 }
