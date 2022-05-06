5dae95793103209aced49436743faafb43213c29
Donna Lemon <donnalemon1@gmail.com>
2022-05-06 12:15:24 -0400

    Ex. 5/Pt. B.

--- src/main/java/com/xpanxion/myapp/student9/ShoppingCart.java
+++ src/main/java/com/xpanxion/myapp/student9/ShoppingCart.java
@@ -2,38 +2,60 @@ package com.xpanxion.myapp.student9;
 
 import java.util.ArrayList;
 
-public class ShoppingCart {
+public class ShoppingCart {  // base class
+    //DATA MEMBERS
     private float price;
-    private int quantity;
+    private Integer quantity;
     private String description;
-    private ArrayList<Item> itemsList;
     private float total;
+    private float shippingCharge;
+    private ArrayList<Item> itemsList;
+    private float salesTax;
+    private float afterTax;
+
+    // CONSTRUCTORS
 
     public ShoppingCart() {
         this.itemsList = new ArrayList<Item>();
+        this.total = total;
+        this.salesTax = 0.1f;
+        this.afterTax= afterTax;
     }
 
-    public ShoppingCart(float shippingFees) {
-
+    public ShoppingCart(Float price, Integer quantity, String description, Float total) {
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+        this.itemsList = new ArrayList<Item>();
     }
+
+    public ShoppingCart(Float shippingCharge) {
+        this.shippingCharge = 5.00f;
+    }
+
+    //METHODS
 
     public void addItem(Item item) {
         this.itemsList.add(item);
-
     }
 
     public void calculateTotal() {
-        float calculator = 0;
-        //for (Item i : itemsList) {
-        //calculator = i.getPrice() * j.getQuantity();
-        for (Item i : itemsList) {
-            for (int j = i.getQuantity(); j > 0; j--) {
-                total += i.getPrice();
-            }
-        }
-    }
 
-    public float getTotal () {
-        return total;
+        for (Item i : itemsList) {
+            total = (total + (i.getPrice() * i.getQuantity()));
+        }
+        if (total < 10.00f) {
+            total += shippingCharge; // total = total + shipping
+        } else if (total > 10.00f) {
+            total = total;
+        }
+        total += (total * salesTax);
     }
+    public float getTotal() {  // Accessors = getter
+
+        return total;
+
+    }
+    // private void itemsList() {
+    // }
 }
