63bff84c1ff2ddc5653419e8335a4c1e5aa06b88
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-05-06 12:10:02 -0400

    ex5b

--- src/main/java/com/xpanxion/myapp/student11/ShoppingCart.java
+++ src/main/java/com/xpanxion/myapp/student11/ShoppingCart.java
@@ -3,21 +3,47 @@ package com.xpanxion.myapp.student11;
 import java.util.ArrayList;
 
 public class ShoppingCart {
+    //
+    //Data members
+    //
+    private float price;
+    private Integer quantity;
+    private String description;
+    private float total;
+    private float shippingCharge;
     private ArrayList<Item> itemList;
-private float total;
+    private float salesTax;
+    private float afterTax;
+
+    //
+    //Constructors
+    //
     public ShoppingCart() {
         this.itemList = new ArrayList<Item>();
+        this.total = total;
+        this.salesTax = 0.1f;
+        this.afterTax = afterTax;
     }
-        public void addItem (Item item){
+
+    public ShoppingCart(float shippingCharge) {
+        this.shippingCharge = 5.00f;
+    }
+
+    public void addItem (Item item){
             this.itemList.add(item);
         }
 
     public void calculateTotal() {
         for (Item i : itemList) {
             total = (total + (i.getPrice() * i.getQuantity()));
-
         }
-    }
+        if (total < 10.00f) {
+            total += shippingCharge;
+        } else if (total > 10.00f) {
+            total = total;
+        }
+        total += (total * salesTax);
+        }
 
     public float getTotal() {
         return total;
